03ff4501c1f9a1af2d31b72f7337db2c41870007
Chuan-Chung Chiang <s22194869@gmail.com>
2017-09-22 09:06:24 +0800

    22th, sep. 2017

---
+++ KanaeFinalTemplate/script.js
@@ -0,0 +1,75 @@
+
+$(document).ready(function(){
+	$('#prd').load('ajax/prdindex.html');
+	$('#prdbest').load('ajax/prdbest.html');
+	
+
+
+////mouse///////////////////////////////////////////////////
+	$("#coat_jack").mouseover(function(){
+    	$("#coat_jack").css("color","#F56777");
+    	$("#coat_jack").css("cursor","pointer");
+  	});
+  	$("#coat_jack").mouseout(function(){
+    	$("#coat_jack").css("color","#333");
+    	$("#coat_jack").css("cursor","default");
+  	});
+  	$("#knit_cardigan").mouseover(function(){
+    	$("#knit_cardigan").css("color","#F56777");
+    	$("#knit_cardigan").css("cursor","pointer");
+  	});
+  	$("#knit_cardigan").mouseout(function(){
+    	$("#knit_cardigan").css("color","#333");
+    	$("#knit_cardigan").css("cursor","default");
+  	});
+  	$("#tee_blouse").mouseover(function(){
+    	$("#tee_blouse").css("color","#F56777");
+    	$("#tee_blouse").css("cursor","pointer");
+  	});
+  	$("#tee_blouse").mouseout(function(){
+    	$("#tee_blouse").css("color","#333");
+    	$("#tee_blouse").css("cursor","default");
+  	});
+  	$("#skirt_pants").mouseover(function(){
+    	$("#skirt_pants").css("color","#F56777");
+    	$("#skirt_pants").css("cursor","pointer");
+  	});
+  	$("#skirt_pants").mouseout(function(){
+    	$("#skirt_pants").css("color","#333");
+    	$("#skirt_pants").css("cursor","default");
+  	});
+  	$("#one_piece").mouseover(function(){
+    	$("#one_piece").css("color","#F56777");
+    	$("#one_piece").css("cursor","pointer");
+  	});
+  	$("#one_piece").mouseout(function(){
+    	$("#one_piece").css("color","#333");
+    	$("#one_piece").css("cursor","default");
+  	});
+  	$("#inf").mouseover(function(){
+    	$("#inf").css("color","#F56777");
+    	$("#inf").css("cursor","pointer");
+  	});
+  	$("#inf").mouseout(function(){
+    	$("#inf").css("color","#333");
+    	$("#inf").css("cursor","default");
+  	});
+  	$("#process").mouseover(function(){
+    	$("#process").css("color","#F56777");
+    	$("#process").css("cursor","pointer");
+  	});
+  	$("#process").mouseout(function(){
+    	$("#process").css("color","#333");
+    	$("#process").css("cursor","default");
+  	});
+  	$("#caution").mouseover(function(){
+    	$("#caution").css("color","#F56777");
+    	$("#caution").css("cursor","pointer");
+  	});
+  	$("#caution").mouseout(function(){
+    	$("#caution").css("color","#333");
+    	$("#caution").css("cursor","default");
+  	});
+////////////////////////////////////////////////////////////
+});
+
